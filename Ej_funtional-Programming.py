715b669cbb157991310d40db360288f92529409a
will <wilmanzuletaen@gmail.com>
2023-08-22 12:11:41 -0500

    actualizando variables ingles

--- Ej_funtional-Programming.py
+++ Ej_funtional-Programming.py
@@ -1,14 +1,14 @@
-numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
 # Función que verifica si un número es par
-def es_par(numero):
-    return numero % 2 == 0
+def is_par(num):
+    return num % 2 == 0
 
 # Función que calcula el cuadrado de un número
-def cuadrado(numero):
-    return numero ** 2
+def scuared(num):
+    return num ** 2
 
 # Usando programación funcional para calcular la suma de los cuadrados de los números pares
-suma_cuadrados_pares = sum(map(cuadrado, filter(es_par, numeros)))
+sum_scuared_pairs = sum(map(scuared, filter(is_par, numbers)))
 
-print(suma_cuadrados_pares)
+print(sum_scuared_pairs)
